f747cd5108983cefeaf1815d4c437bb3be548540
Arjun Ajith <arjuunajith@gmail.com>
2020-06-14 22:40:52 +0530

    artists data

--- National Gallery.py
+++ National Gallery.py
@@ -5,3 +5,20 @@ page = requests.get('https://web.archive.org/web/20121007172955/https://www.nga.
 
 soup = BeautifulSoup(page.text, 'html.parser')
 
+# Remove bottom links
+last_links = soup.find(class_='AlphaNav')
+last_links.decompose()
+
+# Extract artist data 
+artist_name_list = soup.find(class_='BodyText')
+artist_name_list_items = artist_name_list.find_all('a')
+
+# Print artist data
+for artist_name in artist_name_list_items:
+    names = artist_name.contents[0]
+    print(names)
+
+
+
+
+
